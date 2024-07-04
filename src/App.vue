3ec110f6fbbf88fdc0fdac8926df5204ed28d48d
CPS <cp@staszkiewicz.de>
2024-07-04 08:09:43 +0200

    nicer Web interface

--- src/App.vue
+++ src/App.vue
@@ -6,42 +6,63 @@ import FlightDisplay from './flightDisplay.vue';
 </script>
 
 <style scoped>
-.my-flight-display {
-  margin: 10px;
-  padding: 10px;
-  width: 50%;
-  height: 70vh;
+.top {
+  background-color: tomato;
 }
+.my-flight-display {
+  margin-left: 15%;
+  padding: 10px;
+  width: 70%;
+  height: 70vh;
+  color: floralwhite;
+}
+
+.main {
+  padding: 20px;
+  color: floralwhite;
+  font-family: Tahoma, "Trebuchet MS", sans-serif;
+  font-size: 24px;
+}
+
+
+#app {
+  background-color: black;
+}
+h1 {
+  background: linear-gradient(to left ,
+                    violet,
+                    indigo,
+                    blue,
+                    green,
+                    yellow,
+                    orange,
+                    red);
+  -webkit-background-clip: text;
+  color: transparent;
+  font-family: Tahoma, "Trebuchet MS", sans-serif;
+  font-size: 6rem;
+  font-weight: bolder;
+  text-align: center;}
 </style>
 
 <template>
-  <div style="margin: 10px; padding: 10px; width: 50%" class="border border-danger">
-    this is a test  text before 
-    this is a test  text before 
-    this is a test  text before 
-    this is a test  text before 
-    this is a test  text before 
-    this is a test  text before 
-    this is a test  text before 
-    this is a test  text before 
+<div class="top">
+  <div class="main" >
+    <h1>SHOW MY FLIGTHS</h1>
+    The flight-display app lets you display any number of your flight-tracks in a Google Map. Choose the tracks (as gpx-files) from your local storage. 
+    A filter lets you select or deselect all flights of a particular year dynamically. 
+    <p/>
+    This app runs locally in your browser, so your flight data will not be sent over the internet to any server. For each flight track, 
+    the app creates a 'path', consisting of the track points at every minute of your flight. As the departure airport 
+    of a flight typically is identical to the landing airport of the previous flight (if the flights are with the same plane and all flights are recorded), the app, for those 
+    cases, joins both paths if they happen in the same year. This results in a set of paths which is sent the Google Maps service for rendering.
   </div>
-  <FlightDisplay title="Show my Flights" class="my-flight-display border border-danger"></FlightDisplay>
-  <div style="margin: 10px; padding: 10px" class="border border-danger">
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
-    this is a test text after
+  <FlightDisplay  class="my-flight-display"></FlightDisplay>
+  <div class="main">
+    The app is created as a <a href="https://vuejs.org"> Vue </a> component,  see the <a href=https://github.com/staski/flightdisplay>flight-display app on GitHub</a>
+    <p/>
+    Other software tools on <a href="https://fly.venus-flytrap.de/diy">CP's aviation pages</a>
   </div>
+</div>
 </template>
 
